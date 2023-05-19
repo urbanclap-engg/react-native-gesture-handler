d1570e8549f124099c4692e732c443ddd3822d65
pratthamarora <pratthamarora@urbancompany.com>
2023-05-19 13:47:26 +0530

    test build

---
+++ lib/typescript/GestureHandlerRootView.android.d.ts
@@ -0,0 +1,5 @@
+import { GestureHandlerRootViewProps } from './GestureHandlerRootView';
+export default function GestureHandlerRootView({
+  children,
+  ...rest
+}: GestureHandlerRootViewProps): JSX.Element;
---
+++ lib/typescript/components/touchables/TouchableWithoutFeedback.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import GenericTouchable, { GenericTouchableProps } from './GenericTouchable';
+declare const TouchableWithoutFeedback: React.ForwardRefExoticComponent<
+  GenericTouchableProps & {
+    children?: React.ReactNode;
+  } & React.RefAttributes<GenericTouchable>
+>;
+export default TouchableWithoutFeedback;
